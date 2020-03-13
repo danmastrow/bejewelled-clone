54df8bd55c630dd94e6296b89956476bc1e1b580
DANIEL MASTROWICZ <15012375+danmastrow@users.noreply.github.com>
2020-03-13 13:04:36 +1100

    Remove hovering code Change gem models to be unique per color

--- Assets/PlayerInputManager.cs
+++ Assets/PlayerInputManager.cs
@@ -1,13 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerInputManager : MonoBehaviour
 {
     private GemGridManager gemGridManager;
     private Ray ray;
     private RaycastHit hit;
-    private GameObject hoveredObject;
+    //private GameObject hoveredObject;
     private GameObject selectedObject;
     private void Awake()
     {
@@ -16,7 +14,6 @@ public class PlayerInputManager : MonoBehaviour
 
     void Update()
     {
-        var unhover = true;
         if (gemGridManager.GridReady)
         {
             ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -29,30 +26,19 @@ public class PlayerInputManager : MonoBehaviour
                         if (selectedObject == null)
                         {
                             selectedObject = hit.collider.gameObject;
-                        } else if (selectedObject != hit.collider.gameObject)
+                            selectedObject.GetComponent<GemScript>().Hover();
+                        }
+                        else if (selectedObject != hit.collider.gameObject)
                         {
                             var swapSucceeded = gemGridManager.TrySwap(selectedObject, hit.collider.gameObject);
                             if (swapSucceeded)
                             {
-                                hoveredObject.GetComponent<GemScript>().Unhover();
                                 selectedObject.GetComponent<GemScript>().Unhover();
                                 selectedObject = null;
-                                hoveredObject = null;
                             }
                         }
                     }
-                    if (hoveredObject != null && hoveredObject != hit.collider.gameObject)
-                    {
-                        hoveredObject.GetComponent<GemScript>().Unhover();
-                    }
-                    hoveredObject = hit.collider.gameObject;
-                    hoveredObject.GetComponent<GemScript>().Hover();
-                    unhover = false;
                 }
-            }
-            if (hoveredObject != null && unhover)
-            {
-                hoveredObject.GetComponent<GemScript>().Unhover();
             }
         }
     }
